032f4c79c059934521e8a4e5876b6f5144fe4324
Rob Gries <bob2600@gmail.com>
2018-10-24 12:41:34 -0400

    As per Thomas' request, we are now initializing wiphy.max_num_csa_counters to 0

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -510,7 +510,8 @@ use_default_name:
 	rdev->wiphy.rts_threshold = (u32) -1;
 	rdev->wiphy.coverage_class = 0;
 
-	rdev->wiphy.max_num_csa_counters = 1;
+	//Allows CSA via hostapd
+	rdev->wiphy.max_num_csa_counters = 0;
 
 	rdev->wiphy.max_sched_scan_plans = 1;
 	rdev->wiphy.max_sched_scan_plan_interval = U32_MAX;
